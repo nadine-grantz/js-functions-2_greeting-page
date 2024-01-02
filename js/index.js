877c6b99b938a8b7405b66c4830aabbf07b65572
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-02 18:32:53 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -21,13 +21,29 @@ Change the contents of this page depending on the current day and time.
 */
 
 const display = document.querySelector('[data-js="display"]');
+let currentTime = new Date().getHours();
+let currentDay = new Date().getDay();
 
 function getGreeting() {
-  // Code here
+  if (currentTime >= 6 && currentTime <= 12) {
+    return (display.textContent = "Good Morning");
+  } else if (currentTime >= 13 && currentTime <= 18) {
+    return (display.textContent = "Good Afternoon");
+  } else if (currentTime >= 19 && currentTime <= 22) {
+    return (display.textContent = "Good Evening");
+  } else {
+    return (display.textContent = "Good Night");
+  }
 }
 
 function getDayColor() {
-  // Code here
+  if (currentDay === 1) {
+    document.body.style.backgroundColor = "darkgray";
+  } else if (currentDay >= 2 && currentDay <= 5) {
+    document.body.style.backgroundColor = "lightblue";
+  } else {
+    document.body.style.backgroundColor = "hotpink";
+  }
 }
 
 display.textContent = getGreeting();
